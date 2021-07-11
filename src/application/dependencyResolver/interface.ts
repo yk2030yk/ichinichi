bf4574c545baec8887f5c11e7ec788c1baa04d85
yk2030yk <yk.ngsyk@gmail.com>
2021-07-11 19:52:47 +0900

    fix

--- src/application/dependencyResolver/interface.ts
+++
@@ -1,7 +0,0 @@
-import { Dependency } from '@/application/dependency';
-
-export interface IDependencyResolver<T> {
-  resolve: () => T;
-}
-
-export type IApplicationDependencyResolver = IDependencyResolver<Dependency>;
